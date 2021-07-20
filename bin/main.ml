a177ba9ba11fd52ffca0c6b8c17514efde326dc4
JamesTadleyGreen <jamestadleygreen@outlook.com>
2021-07-20 16:17:32 +0100

    Updating the dune file module structure

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
 open Printf
 
-let () = printf "%.*f" 4 (Utility_theory.a (fun x -> x*.x) 4.)
+let () = printf "%.*f" 4 (Cm2.Utility_theory.a (fun x -> x*.x) 4.)
+(* let () = printf "%.*f" 4 (Cm2.Integration.a (fun x -> x*.x) 4.) *)
